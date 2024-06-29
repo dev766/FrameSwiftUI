012b0d32eed01b962844fabb99cb8bd7648b408d
dev766 <akash>
2024-06-29 21:05:05 +0530

    init

--- FrameSwiftUI/ContentView.swift
+++ FrameSwiftUI/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello, World!")
+                    .background(Color.red)
+                    .frame(height: 100, alignment: .top)
+                    .background(Color.orange)
+                    .frame(width: 150)
+                    .background(Color.purple)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(Color.pink)
+                    .frame(height: 400)
+                    .background(Color.green)
+                    .frame(maxHeight: .infinity, alignment: .top)
+                    .background(Color.yellow)
     }
 }
 
