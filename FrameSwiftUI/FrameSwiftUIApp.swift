eba8dbd4904e8adbfa94a2be61512eeca2cd2af0
dev766 <akash>
2024-06-29 20:50:53 +0530

    Initial Commit

---
+++ FrameSwiftUI/FrameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameSwiftUIApp.swift
+//  FrameSwiftUI
+//
+//  Created by Apple on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FrameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
